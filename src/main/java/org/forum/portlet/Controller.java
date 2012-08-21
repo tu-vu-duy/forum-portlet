48440d5f5d824a41ed8252a74679bb1e3d0e244b
tu-vu-duy <tuvd@exoplatform.com>
2012-08-21 17:59:12 +0700

    juzu

--- src/main/java/org/forum/portlet/Controller.java
+++ src/main/java/org/forum/portlet/Controller.java
@@ -1,12 +1,14 @@
 package org.forum.portlet;
 
 import org.exoplatform.web.application.RequestContext;
+import org.exoplatform.forum.ForumUtils;
 import org.exoplatform.forum.service.Category;
 import org.exoplatform.forum.service.ForumService;
+import org.exoplatform.forum.service.Topic;
 import org.forum.portlet.qualifiers.Current;
-import org.forum.portlet.templates.index;
-import org.forum.portlet.templates.categories;
 import org.forum.portlet.utils.Utils;
+import org.forum.portlet.templates.index;
+import org.forum.portlet.templates.category;
 
 import juzu.Action;
 import juzu.Path;
@@ -29,23 +31,37 @@ public class Controller {
   @Inject ForumService forumService;
   
   @Inject @Path("index.gtmpl")    index index;
-  @Inject @Path("categories.gtmpl")    categories categories;
+  @Inject @Path("category.gtmpl")    category category;
+  @Inject @Path("forum.gtmpl")    forum forum;
 
 //  @Inject @SessionScoped Identity currentUser;
 
   @View
   public void index() throws IOException, Exception  {
-    index.render();
+    List<Category> cates = forumService.getCategories();
+    index.with()
+         .categories(cates).render();
   }
 
   @View
-  public void loadForums() throws Exception {
-//    String userId = currentUser.getRemoteId();
-   List<Category> cates = forumService.getCategories();
-   
-   categories.with()
-             .categories(cates).render();
-    
+  public void loadCategory(String cateId) throws Exception {
+    category.with()
+            .category(forumService.getCategory(cateId))
+            .forums(forumService.getForums(cateId, null))
+            .render();
   }
 
+  @View
+  public void loadForum(String cateId, String forumId) throws Exception {
+    forum.with()
+      .forums(forumService.getForum(cateId, forumId)).render();
+  }
+  
+  public Topic getTopic(String topicPath) {
+    Topic topic = null;
+    if (topicPath != null && topicPath.trim().length() > 0) {
+      topic = forumService.getTopicSummary(topicPath);
+    }
+    return topic;
+  }
 }
